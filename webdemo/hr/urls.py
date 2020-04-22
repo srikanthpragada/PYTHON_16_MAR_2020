937979c0544f385fca5dffdf595dfab020f7895e
srikanthpragada <srikanthpragada@gmail.com>
2020-04-22 20:58:45 +0530

    22-APR

--- webdemo/hr/urls.py
+++ webdemo/hr/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
      path("home", views.home),
      path("topics", views.topics),
      path("countries",views.list_countries),
+     path("jobs",views.list_jobs),
+     path("addjob",views.add_job),
 ]
--- webdemo/hr/views.py
+++ webdemo/hr/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import sqlite3
 import requests
 
 
@@ -17,3 +18,35 @@ def list_countries(request):
     resp = requests.get("https://restcountries.eu/rest/v2/all")
     countries = resp.json()  # list of dict
     return render(request, 'list_countries.html', {'countries': countries})
+
+
+def delete_job(request):
+    id = request.GET['id']
+    con = sqlite3.connect(r"c:\classroom\mar16\hr.db")
+    cur = con.cursor()
+    # cur.execute("insert into jobs (title,minsalary) values(?,?)", (title,minsal))
+    con.commit()
+    con.close()
+    return redirect("/hr/jobs")
+
+
+def add_job(request):
+    # if data is passed then process it
+    if 'title' in request.GET:
+        title = request.GET['title']
+        minsal = int(request.GET['minsal'])
+        con = sqlite3.connect(r"c:\classroom\mar16\hr.db")
+        cur = con.cursor()
+        cur.execute("insert into jobs (title,minsalary) values(?,?)", (title, minsal))
+        con.commit()
+        con.close()
+        return redirect("/hr/jobs")
+    else:
+        return render(request, 'add_job.html')
+
+
+def list_jobs(request):
+    con = sqlite3.connect(r"c:\classroom\mar16\hr.db")
+    cur = con.cursor()
+    cur.execute("select * from jobs")
+    return render(request, 'list_jobs.html', {'jobs': cur.fetchall()})
